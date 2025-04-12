59fa284b8124db2175da1ac2330cf95c2feaea3e
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2025-04-12 15:55:26 +0600

    Updated VIew all event api

--- api/Backend-UtshobKotha/Controllers/EventsController.cs
+++ api/Backend-UtshobKotha/Controllers/EventsController.cs
@@ -3,6 +3,7 @@ using Backend_UtshobKotha.Dtos;
 using Backend_UtshobKotha.Models;
 using Backend_UtshobKotha.Validation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace Backend_UtshobKotha.Controllers
@@ -242,6 +243,14 @@ namespace Backend_UtshobKotha.Controllers
                 return NotFound();
             }
             return Ok(eventItem);
+        }
+
+        // Endpoint to view all events
+        [HttpGet]
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var events = await _context.Events.ToListAsync();
+            return Ok(events);
         }
 
 
